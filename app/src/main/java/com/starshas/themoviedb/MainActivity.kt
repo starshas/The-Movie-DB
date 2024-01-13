0fd238c043205f82dbdfa351dfb19242086cd117
alexa <alexander.a.seregin@gmail.com>
2024-01-13 12:07:43 +0100

    Added libraries and enabled viewBinding

--- app/src/main/java/com/starshas/themoviedb/MainActivity.kt
+++ app/src/main/java/com/starshas/themoviedb/MainActivity.kt
@@ -2,10 +2,13 @@ package com.starshas.themoviedb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.starshas.themoviedb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
